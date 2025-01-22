2832041c9ac74b3640dacb7f0e8021aedd1855c9
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-01-21 22:44:21 -0800

    halfway done - up to MovieNavBar

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 function MovieDetails() {
+  const { movieId } = useParams();
+  console.log("movieId:", movieId);
+
   return (
     <div className='comp purple'>
       <h1>MovieDetails Component</h1>
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,13 @@
-function Movies() {
+import { Outlet } from 'react-router-dom';
+
+function Movies({ movies }) {
+  // const { movies } = useParams();
+  console.log(movies);
+
   return (
     <div className="comp orange">
       <h1>Movies Component</h1>
+      <Outlet />
     </div>
   );
 }
